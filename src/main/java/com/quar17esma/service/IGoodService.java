61cf59bbfad09f3a8b6b2a0d48fc0eb401cc3f29
quar17esma <quar17esma@gmail.com>
2018-02-08 03:38:53 +0200

    Added GoodService interface

---
+++ src/main/java/com/quar17esma/service/IGoodService.java
@@ -0,0 +1,21 @@
+package com.quar17esma.service;
+
+import com.quar17esma.model.Good;
+import com.quar17esma.model.Order;
+
+import java.util.List;
+
+public interface IGoodService {
+
+    List<Good> findAll();
+
+    int countAll();
+
+    Good findById(Long goodId);
+
+    void deleteById(Long goodId);
+
+    void save(Good good);
+
+    void addGoodToOrder(Order order, Long goodId, int orderedQuantity);
+}
