920aca028ad4fdff62a6ec222e78cd1070fe8e81
quar17esma <quar17esma@gmail.com>
2018-02-10 06:02:25 +0200

    added Controller

---
+++ src/main/java/com/quar17esma/controller/MyController.java
@@ -0,0 +1,35 @@
+package com.quar17esma.controller;
+
+import com.quar17esma.model.Good;
+import com.quar17esma.service.IGoodService;
+import com.quar17esma.service.impl.GoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class MyController {
+
+    @Autowired
+    IGoodService service;
+
+    @Autowired
+    MessageSource messageSource;
+
+    /*
+     * List all existing Goods.
+     */
+    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+    public String listGoods(ModelMap model) {
+
+        List<Good> goods = service.findAll();
+        model.addAttribute("goods", goods);
+        return "allgoods";
+    }
+}
