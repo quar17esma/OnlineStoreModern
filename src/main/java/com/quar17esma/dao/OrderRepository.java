07e3dbbd0654f98c633ed0afd71d67b4fdc105b3
quar17esma <quar17esma@gmail.com>
2018-02-19 01:00:17 +0200

    deleted comment

--- src/main/java/com/quar17esma/dao/OrderRepository.java
+++ src/main/java/com/quar17esma/dao/OrderRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-//    List<Order> findAllByClientId(Long clientId);
     List<Order> findAllByUserId(Long userId);
 }
