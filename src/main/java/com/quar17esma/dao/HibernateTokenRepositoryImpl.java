e64d6f4376f549d710bc052be72bebf08c0dd5f7
quar17esma <quar17esma@gmail.com>
2018-02-19 02:38:04 +0200

    fixed token work

--- src/main/java/com/quar17esma/dao/HibernateTokenRepositoryImpl.java
+++ src/main/java/com/quar17esma/dao/HibernateTokenRepositoryImpl.java
@@ -2,62 +2,65 @@ package com.quar17esma.dao;
 
 import com.quar17esma.model.PersistentLogin;
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Date;
 
 
 @Repository("tokenRepositoryDao")
 @Transactional
-public class HibernateTokenRepositoryImpl extends AbstractDao<String, PersistentLogin>
-		implements PersistentTokenRepository {
+public class HibernateTokenRepositoryImpl implements PersistentTokenRepository {
 
-	@Override
-	public void createNewToken(PersistentRememberMeToken token) {
-		PersistentLogin persistentLogin = new PersistentLogin();
-		persistentLogin.setUsername(token.getUsername());
-		persistentLogin.setSeries(token.getSeries());
-		persistentLogin.setToken(token.getTokenValue());
-		persistentLogin.setLast_used(token.getDate());
-		persist(persistentLogin);
+    @PersistenceContext
+    private EntityManager em;
 
-	}
+    @Override
+    public void createNewToken(PersistentRememberMeToken token) {
+        PersistentLogin persistentLogin = new PersistentLogin();
+        persistentLogin.setUsername(token.getUsername());
+        persistentLogin.setSeries(token.getSeries());
+        persistentLogin.setToken(token.getTokenValue());
+        persistentLogin.setLast_used(token.getDate());
+        em.persist(persistentLogin);
+    }
 
-	@Override
-	public PersistentRememberMeToken getTokenForSeries(String seriesId) {
-		try {
-			Criteria crit = createEntityCriteria();
-			crit.add(Restrictions.eq("series", seriesId));
-			PersistentLogin persistentLogin = (PersistentLogin) crit.uniqueResult();
+    @Override
+    public PersistentRememberMeToken getTokenForSeries(String seriesId) {
+        try {
+            Criteria crit = em.unwrap(Session.class).createCriteria(PersistentLogin.class);
+            crit.add(Restrictions.eq("series", seriesId));
+            PersistentLogin persistentLogin = (PersistentLogin) crit.uniqueResult();
 
-			return new PersistentRememberMeToken(persistentLogin.getUsername(), persistentLogin.getSeries(),
-					persistentLogin.getToken(), persistentLogin.getLast_used());
-		} catch (Exception e) {
-			return null;
-		}
-	}
+            return new PersistentRememberMeToken(persistentLogin.getUsername(), persistentLogin.getSeries(),
+                    persistentLogin.getToken(), persistentLogin.getLast_used());
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-	@Override
-	public void removeUserTokens(String username) {
-		Criteria crit = createEntityCriteria();
-		crit.add(Restrictions.eq("username", username));
-		PersistentLogin persistentLogin = (PersistentLogin) crit.uniqueResult();
-		if (persistentLogin != null) {
-			delete(persistentLogin);
-		}
+    @Override
+    public void removeUserTokens(String username) {
+        Criteria crit = em.unwrap(Session.class).createCriteria(PersistentLogin.class);
+        crit.add(Restrictions.eq("username", username));
+        PersistentLogin persistentLogin = (PersistentLogin) crit.uniqueResult();
+        if (persistentLogin != null) {
+            em.remove(persistentLogin);
+        }
 
-	}
+    }
 
-	@Override
-	public void updateToken(String seriesId, String tokenValue, Date lastUsed) {
-		PersistentLogin persistentLogin = getByKey(seriesId);
-		persistentLogin.setToken(tokenValue);
-		persistentLogin.setLast_used(lastUsed);
-		update(persistentLogin);
-	}
-
+    @Override
+    public void updateToken(String seriesId, String tokenValue, Date lastUsed) {
+        PersistentLogin persistentLogin = em.find(PersistentLogin.class, seriesId);
+        persistentLogin.setToken(tokenValue);
+        persistentLogin.setLast_used(lastUsed);
+        em.persist(persistentLogin);
+    }
 }
