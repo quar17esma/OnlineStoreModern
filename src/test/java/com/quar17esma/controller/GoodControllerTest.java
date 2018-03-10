39726b410a0e05bf4f069a72e8c57c089cfcadfd
quar17esma <quar17esma@gmail.com>
2018-03-10 05:57:49 +0200

    added listGoods and newGoodValidation tests

--- src/test/java/com/quar17esma/controller/GoodControllerTest.java
+++ src/test/java/com/quar17esma/controller/GoodControllerTest.java
@@ -1,28 +1,32 @@
 package com.quar17esma.controller;
 
 import com.quar17esma.configuration.AppConfig;
+import com.quar17esma.model.Good;
 import com.quar17esma.service.GoodService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
@@ -34,7 +38,10 @@ public class GoodControllerTest {
     private GoodService goodServiceMock;
 
     @Mock
-    private UserController userController;
+    private UserController userControllerMock;
+
+    @Mock
+    private MessageSource messageSourceMock;
 
     @InjectMocks
     private GoodController controller;
@@ -43,24 +50,99 @@ public class GoodControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
+//        Mockito.reset(goodServiceMock, userControllerMock, messageSourceMock);
+
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
 
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setValidator(validator)
                 .setViewResolvers(viewResolver)
                 .build();
     }
 
-    @Ignore
+    //    @Ignore
     @Test
     public void listGoods() throws Exception {
-        when(goodServiceMock.findAll()).thenReturn(new ArrayList<>());
-        when(userController.getPrincipal()).thenReturn("");
+
+        Good firstGood = Good.builder()
+                .id(1L)
+                .name("Lorem")
+                .description("Lorem ipsum")
+                .price(200L)
+                .quantity(34)
+                .build();
+
+        Good secondGood = Good.builder()
+                .id(2L)
+                .name("ipsum")
+                .description("Lorem ipsum")
+                .price(500L)
+                .quantity(23)
+                .build();
+
+        when(goodServiceMock.findAll()).thenReturn(Arrays.asList(firstGood, secondGood));
+        when(userControllerMock.getPrincipal()).thenReturn("johnny");
 
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("allGoods"));
+                .andExpect(view().name("allGoods"))
+                .andExpect(forwardedUrl("/WEB-INF/views/allGoods.jsp"))
+                .andExpect(model().attribute("goods", hasSize(2)))
+                .andExpect(model().attribute("goods", hasItem(
+                        allOf(
+                                hasProperty("id", is(1L)),
+                                hasProperty("description", is("Lorem ipsum")),
+                                hasProperty("name", is("Lorem")),
+                                hasProperty("price", is(200L)),
+                                hasProperty("quantity", is(34))
+                        )
+                )))
+                .andExpect(model().attribute("goods", hasItem(
+                        allOf(
+                                hasProperty("id", is(2L)),
+                                hasProperty("description", is("Lorem ipsum")),
+                                hasProperty("name", is("ipsum")),
+                                hasProperty("price", is(500L)),
+                                hasProperty("quantity", is(23))
+                        )
+                )));
+        verify(goodServiceMock, times(1)).findAll();
+        verifyNoMoreInteractions(goodServiceMock);
     }
 
+    //    @Ignore
+    @Test
+    public void saveNewGoodValidationFail() throws Exception {
+        when(userControllerMock.getPrincipal()).thenReturn("johnny");
+
+        String name = StringUtils.repeat("a", 102);
+        String description = StringUtils.repeat("a", 1001);
+        Long price = 100L;
+        Integer quantity = 50;
+        Long id = 0L;
+
+        mockMvc.perform(post("/newgood")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", name)
+                .param("description", description)
+                .param("price", String.valueOf(price))
+                .param("quantity", String.valueOf(quantity))
+                .requestAttr("good", new Good())
+        )
+                .andExpect(status().isOk())
+                .andExpect(view().name("editGood"))
+                .andExpect(forwardedUrl("/WEB-INF/views/editGood.jsp"))
+                .andExpect(model().attributeHasFieldErrors("good", "name"))
+                .andExpect(model().attributeHasFieldErrors("good", "description"))
+                .andExpect(model().attributeErrorCount("good", 2))
+                .andExpect(model().attribute("good", hasProperty("id", is(id))))
+                .andExpect(model().attribute("good", hasProperty("description", is(description))))
+                .andExpect(model().attribute("good", hasProperty("name", is(name))));
+
+        verifyZeroInteractions(goodServiceMock);
+    }
 }
