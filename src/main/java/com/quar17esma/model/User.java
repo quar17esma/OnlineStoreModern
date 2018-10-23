5b01c507681613cf202b5b34677eb4d631a72f81
quar17esma <quar17esma@gmail.com>
2018-10-23 18:17:32 +0300

    added builder to user

--- src/main/java/com/quar17esma/model/User.java
+++ src/main/java/com/quar17esma/model/User.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(exclude = "password")
+@Builder
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
