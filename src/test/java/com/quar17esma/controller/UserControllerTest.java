4f529a7a932f0e83df86b63575d6a56083780778
quar17esma <quar17esma@gmail.com>
2018-10-25 01:18:24 +0300

    added accessDeniedPage test

--- src/test/java/com/quar17esma/controller/UserControllerTest.java
+++ src/test/java/com/quar17esma/controller/UserControllerTest.java
@@ -1,12 +1,16 @@
 package com.quar17esma.controller;
 
 import com.quar17esma.configuration.AppConfig;
+import com.quar17esma.enums.UserProfileType;
+import com.quar17esma.model.User;
+import com.quar17esma.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.context.MessageSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,6 +20,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 @WebAppConfiguration
@@ -23,8 +32,11 @@ public class UserControllerTest {
     private MockMvc mockMvc;
 
     @Mock
+    private UserService userService;
+    @Mock
     private MessageSource messageSourceMock;
 
+    @Spy
     @InjectMocks
     private UserController controller;
 
@@ -47,6 +59,27 @@ public class UserControllerTest {
 
     @Test
     public void accessDeniedPage() throws Exception {
+        User user = createTestUser();
+        when(userService.findByEmail(user.getEmail())).thenReturn(user);
+        doReturn(user.getEmail()).when(controller).getPrincipal();
+
+        mockMvc.perform(
+                get("/Access_Denied"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("accessDenied"))
+                .andExpect(forwardedUrl("/WEB-INF/views/templates/accessDenied.html"))
+                .andExpect(model().attribute("userFirstName", is(user.getFirstName())));
+        verify(userService, times(1)).findByEmail(user.getEmail());
+    }
+
+    private User createTestUser() {
+        return  User.builder()
+                .id(10L)
+                .email("test@gmail.com")
+                .firstName("John")
+                .lastName("Smith")
+                .userProfileType(UserProfileType.USER)
+                .build();
     }
 
     @Test
