b2753406a788812bc00c1adab689e24ce0b74fd7
quar17esma <quar17esma@gmail.com>
2018-10-26 15:12:20 +0300

    Enabled Global Method Security

---
+++ src/main/java/com/quar17esma/security/MethodSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.quar17esma.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+}
