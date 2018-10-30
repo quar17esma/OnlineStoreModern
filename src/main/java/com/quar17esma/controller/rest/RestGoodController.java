bf74c33771fae51f06aa37ee9b0472e33d91cc9d
quar17esma <quar17esma@gmail.com>
2018-10-30 15:41:02 +0200

    added RestGoodController with goodById

--- src/main/java/com/quar17esma/controller/rest/RestGoodController.java
+++ src/main/java/com/quar17esma/controller/rest/RestGoodController.java
@@ -1,12 +1,12 @@
 package com.quar17esma.controller.rest;
 
+import com.quar17esma.exceptions.GoodNotFoundException;
 import com.quar17esma.model.Good;
 import com.quar17esma.service.GoodService;
+import com.quar17esma.exceptions.MyRestError;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest/goods")
@@ -18,7 +18,14 @@ public class RestGoodController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Good goodById(@PathVariable long id) {
         Good good = goodService.findById(id);
-
+        if (good == null) { throw new GoodNotFoundException(id); }
         return good;
+    }
+
+    @ExceptionHandler(GoodNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MyRestError goodNotFound(GoodNotFoundException e) {
+        long goodId = e.getGoodId();
+        return new MyRestError(404, "Good [" + goodId + "] not found");
     }
 }
